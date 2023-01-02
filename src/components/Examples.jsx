8cee2bf9fe68bae29111fc60e10a6e215251ddfc
VictuallyIAm <vicbek8@gmail.com>
2023-01-02 17:54:46 +0600

    added content

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react'
 import beatsite from '../data/beatsite.png'
 import schoolsite from '../data/schoolsite.png'
+import plagsite from '../data/plagsite.png'
 
 const Examples = () => {
   return (
@@ -13,7 +14,7 @@ const Examples = () => {
       </h1>
       <div className="flex flex-wrap md:justify-center">
         <div className="border-zinc-200 border-1 bg-gray-200  w-full mx-4 mb-3 mt-0  rounded text-black p-2 flex justify-start md:flex-col ">
-          <div className="p-3 z-30 w-96 flex ">
+          <div className="p-3 z-20 w-96">
             <img src={beatsite} className="rounded" alt="logo" />
           </div>
           <div className="mx-4 flex flex-col  justify-start items-start">
@@ -57,8 +58,33 @@ const Examples = () => {
               </button>
             </a>
           </div>
-          <div className="p-3 z-30 w-96 flex ">
+          <div className="p-3 z-20 w-96">
             <img src={schoolsite} className="rounded" alt="logo" />
+          </div>
+        </div>
+        <div className="border-zinc-200 border-1 bg-gray-200  w-full mx-4 mb-3 mt-0  rounded text-black p-2 flex justify-start md:flex-col ">
+          <div className="p-3 z-20 w-96">
+            <img src={plagsite} className="rounded" alt="logo" />
+          </div>
+          <div className="mx-4 flex flex-col  justify-start items-start">
+            <div className="text-3xl font-bold border-b-2">
+              E-commerce and news <applet></applet>
+            </div>
+            <div className="my-4 text-left">
+              Here you can find out some info about the company and order some
+              stuff. I also added some fun things with overflow. Nothing
+              serious, just check it out. Dont forget to try it on your phone as
+              well...
+            </div>
+            <a
+              href="https://victuallyiam.github.io/plague-app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="px-10 py-2 border-2 bg-sky-500 rounded-xl">
+                Visit
+              </button>
+            </a>
           </div>
         </div>
       </div>
